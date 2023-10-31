3ae670d12c50e8fc0d7140d224e915d13ac4b50b
kaouther <kaoutherboutheldja@gmail.com>
2023-10-31 20:07:43 +0100

    Add get float file

---
+++ getFloat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * getFloat - function that get an float
+ * 
+ * @message: a string message 
+ * Return: a float
+*/
+float getFloat(char *message)
+{
+    int input;
+    printf("%s",message);
+    scanf("%d",&input);
+    return input;
+}
