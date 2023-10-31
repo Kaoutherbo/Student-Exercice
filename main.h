418a1cccb0d068cb7badfe164c5334287253de5f
kaouther <kaoutherboutheldja@gmail.com>
2023-10-31 20:08:49 +0100

    Add main.h file

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Header files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Structures
+typedef struct Marks Marks;
+struct Marks{
+    float mark;
+    int coef;
+    struct Marks *nextMark; 
+};
+
+typedef struct Student Student;
+
+struct Student{
+    int ID;
+    char *fName;
+    char *lName;
+    float average;
+    Marks *mark;
+    struct Student *nextStudent;
+};
+
+// prototypes of functions
+int getInt(char *message);
+int getChar(char *message);
+int getFloat(char *message);
+Student *createStudent(int);
+Student *addStudent(Student *, int);
+Marks *createMarks(int);
+
+
+#endif
