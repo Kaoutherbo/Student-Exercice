fc991f1eed5e34d6e362aac2634ce15730851bfb
kaouther <kaoutherboutheldja@gmail.com>
2023-11-01 22:53:09 +0100

    Add description

--- calculateAverage.c
+++ calculateAverage.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * calculateAverage - function that calculate the average of each student
+ * 
+ * @head: pointer to the mark list of student
+ * 
+ * Return: average of student 
+*/
+
 float calculateAverage(Marks *head)
 {
     float average = 0, sumMarks = 0;
